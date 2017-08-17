923e3c7bc3f66656eaff081a6c105f862f7b3bd5
Maurice <maurice@maurices-macbook-pro.local>
2017-08-17 15:52:07 +0800

    Initial Commit

---
+++ MyPlayApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPlayApp2
+//
+//  Created by Maurice on 17/8/17.
+//  Copyright © 2017 Maurice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
