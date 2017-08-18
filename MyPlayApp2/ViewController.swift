0bb6bfef2dfc6efcf8c3c35f846cb23aa49a2285
Maurice <maurice@Maurices-MacBook-Pro.local>
2017-08-19 00:43:15 +0800

    Added new files to test

--- MyPlayApp2/ViewController.swift
+++ MyPlayApp2/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var uiFitbit: UIImageView!
+    @IBOutlet weak var uiMinecraft: UIImageView!
+    @IBOutlet weak var uiFitbitButton: UIButton!
+    @IBOutlet weak var uiMineCraftButton: UIButton!
+    
+    @IBOutlet weak var uiStartButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        uiFitbitButton.isHidden = true
+        uiMineCraftButton.isHidden = true
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func uiStartButtonAction(_ sender: Any) {
+        
+        uiMineCraftButton.isHidden = false
+        uiFitbitButton.isHidden = false
+        uiStartButton.isHidden = true
+    }
+    
+    @IBAction func uiFitbitAction(_ sender: Any) {
+        uiFitbit.isHidden = false
+        uiMinecraft.isHidden = true
+    }
+    
+    @IBAction func uiMinecraftAction(_ sender: Any) {
+        uiFitbit.isHidden = true
+        uiMinecraft.isHidden = false
+
+    }
 
 }
 
